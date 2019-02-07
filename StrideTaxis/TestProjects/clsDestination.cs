4111942a821e0347de9484c1f074652000efdee0
Kevin Obeng <P1722971X@my365.dmu.ac.uk>
2019-02-07 20:38:47 +0000

    Properties for clsDestination Test made

--- StrideTaxis/TestProjects/clsDestination.cs
+++ StrideTaxis/TestProjects/clsDestination.cs
@@ -1,9 +1,18 @@
-﻿namespace TestProjects
+﻿using System;
+
+namespace TestProjects
 {
     internal class clsDestination
     {
         public clsDestination()
         {
         }
+
+        public DateTime DropoffTime { get; internal set; }
+        public int EndPointHouseNo { get; internal set; }
+        public string EndPointPostCode { get; internal set; }
+        public string EndPointStreet { get; internal set; }
+        public string EndPointTown { get; internal set; }
+        public DateTime PickupTime { get; internal set; }
     }
 }
--- StrideTaxis/TestProjects/tstDestination.cs
+++ StrideTaxis/TestProjects/tstDestination.cs
@@ -14,5 +14,78 @@ namespace TestProjects
             //test to see that it exists
             Assert.IsNotNull(AnDestination);
         }
+        [TestMethod]
+        public void DropoffTimePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsDestination AnDestination = new clsDestination();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnDestination.DropoffTime = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnDestination.DropoffTime, TestData);
+        }
+        [TestMethod]
+        public void EndPointHouseNoPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsDestination AnDestination = new clsDestination();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnDestination.EndPointHouseNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnDestination.EndPointHouseNo, TestData);
+        }
+        [TestMethod]
+        public void EndPointPostCodePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsDestination AnDestination = new clsDestination();
+            //create some test data to assign to the property
+            String TestData = "LE1 9BH";
+            //assign the data to the property
+            AnDestination.EndPointPostCode = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnDestination.EndPointPostCode, TestData);
+        }
+        [TestMethod]
+        public void EndPointStreetPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsDestination AnDestination = new clsDestination();
+            //create some test data to assign to the property
+            String TestData = "Regent Street";
+            //assign the data to the property
+            AnDestination.EndPointStreet = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnDestination.EndPointStreet, TestData);
+        }
+        [TestMethod]
+        public void EndPointTownPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsDestination AnDestination = new clsDestination();
+            //create some test data to assign to the property
+            String TestData = "Loughborough";
+            //assign the data to the property
+            AnDestination.EndPointTown = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnDestination.EndPointTown, TestData);
+        }
+
+        [TestMethod]
+        public void PickupTimePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsDestination AnDestination = new clsDestination();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnDestination.PickupTime = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnDestination.PickupTime, TestData);
+        }
     }
 }
