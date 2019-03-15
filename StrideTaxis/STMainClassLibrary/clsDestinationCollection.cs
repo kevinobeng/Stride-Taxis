95ae72af01873c7c713dfa6dca1dc586feea8d33
Kevin Obeng <P1722971X@my365.dmu.ac.uk>
2019-03-15 17:51:54 +0000

    edits made

--- StrideTaxis/STMainClassLibrary/clsDestinationCollection.cs
+++ StrideTaxis/STMainClassLibrary/clsDestinationCollection.cs
@@ -1,25 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace STMainClassLibrary
 {
     public class clsDestinationCollection
     {
-        //private data nember for the list
-        List<clsDestination> mDestinationList = new List<clsDestination>();
+        //private data member that stores the count of records found
+        private Int32 mRecordCount;
+        //create a private list data member to store the data from the database
+        private List<clsDestination> mDestinationList = new List<clsDestination>();
+        //private data member to connect to the database
+        private clsDataConnection myDB = new clsDataConnection();
+
+        //public property returning the count of records
+        public Int32 Count
+        {
+            //getter for the property
+            get
+            {
+                //return record count
+                return mRecordCount;
+            }
+        }
+        //public List of destinations
         public List<clsDestination> DestinationList
         {
             get
             {
-                //return the private data
+                //return private dat
                 return mDestinationList;
             }
             set
             {
-                //set the private data 
+                //set the private data
                 mDestinationList = value;
             }
         }
+
         public clsDestination ThisDestination { get; set; }
-        public int Count { get; set; }
+
     }
 }
