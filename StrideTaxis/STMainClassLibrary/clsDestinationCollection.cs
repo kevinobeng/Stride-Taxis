33b0f8638d387dd0e821f1af0d5592368aaa59dc
Kevin Obeng <P1722971X@my365.dmu.ac.uk>
2019-03-15 20:56:29 +0000

    collection tests edits

--- StrideTaxis/STMainClassLibrary/clsDestinationCollection.cs
+++ StrideTaxis/STMainClassLibrary/clsDestinationCollection.cs
@@ -9,12 +9,11 @@ namespace STMainClassLibrary
     public class clsDestinationCollection
     {
         //private data member that stores the count of records found
-        private Int32 mRecordCount;
+        private Int32 mCount;
         //create a private list data member to store the data from the database
         private List<clsDestination> mDestinationList = new List<clsDestination>();
         //private data member to connect to the database
         private clsDataConnection myDB = new clsDataConnection();
-
         //public property returning the count of records
         public Int32 Count
         {
@@ -22,7 +21,11 @@ namespace STMainClassLibrary
             get
             {
                 //return record count
-                return mRecordCount;
+                return mDestinationList.Count;
+            }
+            set
+            {
+
             }
         }
         //public List of destinations
@@ -39,8 +42,46 @@ namespace STMainClassLibrary
                 mDestinationList = value;
             }
         }
-
         public clsDestination ThisDestination { get; set; }
 
+        //constructor for the class
+        public clsDestinationCollection()
+        {
+            //var for the index
+            Int32 Index = 0;
+            //var to store the record count 
+            Int32 RecordCount = 0;
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblDestination_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //While there are record are records to process
+            while (Index < RecordCount)
+            {
+                //create a blank Destination
+                clsDestination AnDestination = new clsDestination();
+                //read in the fields from the current record
+                AnDestination.DestinationID = Convert.ToInt32(DB.DataTable.Rows[0]["DestinationID"]);
+                //private Int32 PickupTime;
+                AnDestination.PickupTime = Convert.ToDateTime(DB.DataTable.Rows[0]["PickupTime"]);
+                //private Int32 EndPointHouseNo;
+                AnDestination.EndPointHouseNo = Convert.ToInt32(DB.DataTable.Rows[0]["EndPointHouseNo"]);
+                //private string EndPointPostCode;
+                AnDestination.EndPointPostCode = Convert.ToString(DB.DataTable.Rows[0]["EndPointPostCode"]);
+                //private string EndPointStreet;
+                AnDestination.EndPointStreet = Convert.ToString(DB.DataTable.Rows[0]["EndPointStreet"]);
+                //private string EndPointTown;
+                AnDestination.EndPointTown = Convert.ToString(DB.DataTable.Rows[0]["EndPointTown"]);
+                //private string DropoffTime;
+                AnDestination.DropoffTime = Convert.ToDateTime(DB.DataTable.Rows[0]["DropoffTime"]);
+                //add the record to the private data member
+                mDestinationList.Add(AnDestination);
+                //point at the next record
+                Index++;
+
+            }
+        }
     }
 }
