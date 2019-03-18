ffa03c150708167dbe7792dfa3bcfdcb10c06fda
p17160065 <p17160065@GWHXX652WIN005W>
2019-03-18 17:01:44 +0000

    Valid

--- StrideTaxis/STMainClassLibrary/clsCustomer.cs
+++ StrideTaxis/STMainClassLibrary/clsCustomer.cs
@@ -1,4 +1,6 @@
-﻿namespace STMainClassLibrary
+﻿using System;
+
+namespace STMainClassLibrary
 {
     public class clsCustomer
     {
@@ -7,5 +9,14 @@
         public string LastName { get; set; }
         public int HouseNumber { get; set; }
         public string Town { get; set; }
+        public string Street { get; set; }
+        public string PostCode { get; set; }
+        public string Email { get; set; }
+        public int TelephoneNumber { get; set; }
+
+        public string UserNumberValid(string someUserNumber)
+        {
+            return "";
+        }
     }
 }
---
+++ StrideTaxis/STMainClassLibrary/clsDestinationCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace STMainClassLibrary
+{
+    public class clsDestinationCollection
+    {
+        public List<clsDestination> DestinationList { get; set; }
+        public clsDestination ThisDestination { get; set; }
+        public int Count { get; set; }
+    }
+}
