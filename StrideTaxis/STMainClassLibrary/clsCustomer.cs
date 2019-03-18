712554c658cfe359b1b4f0f76321af6fd6f6f8b8
p17160065 <p17160065@GWHXX652WIN005W>
2019-03-18 15:54:21 +0000

    Testing

---
+++ StrideTaxis/STMainClassLibrary/clsCustomer.cs
@@ -0,0 +1,11 @@
+﻿namespace STMainClassLibrary
+{
+    public class clsCustomer
+    {
+        public int UserNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int HouseNumber { get; set; }
+        public string Town { get; set; }
+    }
+}
--- StrideTaxis/TestProjects/tstCustomer.cs
+++ StrideTaxis/TestProjects/tstCustomer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using STMainClassLibrary;
 
 namespace TestProjects
 {
@@ -9,7 +10,127 @@ namespace TestProjects
         [TestMethod]
         public void InstanceOK()
         {
-            //create
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+
+        [TestMethod]
+        public void UserNumberOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test for the data
+            Int32 SomeUserNumber = 1;
+            //asign the data to the property
+            ACustomer.UserNumber = SomeUserNumber;
+                    //test to see that it exists
+            Assert.AreEqual(ACustomer.UserNumber, SomeUserNumber);
+        }
+
+        [TestMethod]
+        public void FirstNameOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test for the data
+            String SomeFirstName = "Hanna";
+            //asign the data to the property
+            ACustomer.FirstName = SomeFirstName;
+            //test to see that it exists
+            Assert.AreEqual(ACustomer.FirstName, SomeFirstName);
+        }
+
+        [TestMethod]
+        public void LastNameOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test for the data
+            String SomeLastName = "Burrows";
+            //asign the data to the property
+            ACustomer.LastName = SomeLastName;
+            //test to see that it exists
+            Assert.AreEqual(ACustomer.LastName, SomeLastName);
+        }
+
+        [TestMethod]
+        public void HouseNumberOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test for the data
+            Int32 SomeHouseNumber = 1;
+            //asign the data to the property
+            ACustomer.HouseNumber = SomeHouseNumber;
+            //test to see that it exists
+            Assert.AreEqual(ACustomer.HouseNumber, SomeHouseNumber);
+        }
+
+        [TestMethod]
+        public void TownOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test for the data
+            String SomeTown = "Leicestershire";
+            //asign the data to the property
+            ACustomer.Town = SomeTown;
+            //test to see that it exists
+            Assert.AreEqual(ACustomer.Town, SomeTown);
+        }
+
+        [TestMethod]
+        public void StreetOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test for the data
+            Int32 SomeUserNumber = 1;
+            //asign the data to the property
+            ACustomer.UserNumber = SomeUserNumber;
+            //test to see that it exists
+            Assert.AreEqual(ACustomer.UserNumber, SomeUserNumber);
+        }
+
+        [TestMethod]
+        public void PostCodeOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test for the data
+            Int32 SomeUserNumber = 1;
+            //asign the data to the property
+            ACustomer.UserNumber = SomeUserNumber;
+            //test to see that it exists
+            Assert.AreEqual(ACustomer.UserNumber, SomeUserNumber);
+        }
+
+        [TestMethod]
+        public void EmailOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test for the data
+            Int32 SomeUserNumber = 1;
+            //asign the data to the property
+            ACustomer.UserNumber = SomeUserNumber;
+            //test to see that it exists
+            Assert.AreEqual(ACustomer.UserNumber, SomeUserNumber);
+        }
+
+        [TestMethod]
+        public void TelephoneNumberOK()
+        {
+            //create an instance of the class
+            clsCustomer ACustomer = new clsCustomer();
+            //create a test for the data
+            Int32 SomeUserNumber = 1;
+            //asign the data to the property
+            ACustomer.UserNumber = SomeUserNumber;
+            //test to see that it exists
+            Assert.AreEqual(ACustomer.UserNumber, SomeUserNumber);
         }
     }
 }
